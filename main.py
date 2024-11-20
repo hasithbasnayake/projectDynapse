97055ef327e4a3345021f54f72dfd3a954e41a70
hasithbasnayake <hasith@ucsb.edu>
2024-11-19 17:53:16 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# .\env\Scripts\activate to activate virtual environment
+# pip freeze > requirements.txt You can export a list of all installed packages
+# pip install -r requirements.txt to install from a requirements file
+# pip list to list all packages
+
+import numpy as np
+import matplotlib as plt
+
+
