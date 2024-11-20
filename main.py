2d7cb6cc1a83f71fda78b4edecd9aefea14e463e
hasithbasnayake <hasith@ucsb.edu>
2024-11-19 17:57:44 -0800

    Set dimensions and generate difference of gaussians kernels

--- main.py
+++ main.py
@@ -7,4 +7,18 @@ import numpy as np
 import matplotlib as plt
 from func import *
 
+dim = np.array([6, 6])  # kernel dimensions (in pixels)
+ppa = np.array([8, 8])  # pixels per arc (scaling factor)
+ang = np.ceil(dim / ppa)  # angular size based on pixels per arc
+ctr = (1/3) * dim[0]  # center size as a fraction of kernel size
+sur = (2/3) * dim[0]  # surround size as a fraction of kernel size
+
+kernelON = dogKernel(dim = dim, ang = ang, ppa = ppa, ctr = ctr, sur = sur)
+kernelOFF = dogKernel(dim = dim, ang = ang, ppa = ppa, ctr = ctr, sur = sur)
+
+kernelON.setFilterCoefficients(ONOFF="ON")
+kernelOFF.setFilterCoefficients(ONOFF="OFF")
+
+kernelON.displayKernel(show_plt=True, show_hist=True)
+
 
