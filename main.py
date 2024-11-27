1daa7833bdd72c9bfd461b8858d5a27d1fb6f073
hasithbasnayake <hasith@ucsb.edu>
2024-11-26 22:01:19 -0800

    Create model class

--- main.py
+++ main.py
@@ -5,8 +5,8 @@
  
 from torchvision import datasets
 from torch.utils.data import DataLoader
+import torch.nn as nn
 import snntorch as snn
-from IPython.display import HTML
 from snntorch import spikegen
 import snntorch.spikeplot as splt
 import numpy as np
@@ -93,3 +93,34 @@ beta = 0.95
 
 batch_size = 100
 epochs = 5
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # Initialize layers
+        self.fc1 = nn.Linear(num_inputs, num_hidden)
+        self.lif1 = snn.Leaky(beta=beta)
+        self.fc2 = nn.Linear(num_hidden, num_outputs)
+        self.lif2 = snn.Leaky(beta=beta)
+
+    def forward(self, x):
+
+        # Initialize hidden states at t=0
+        mem1 = self.lif1.init_leaky()
+        mem2 = self.lif2.init_leaky()
+
+        # Record the final layer
+        spk2_rec = []
+        mem2_rec = []
+
+        for step in range(num_steps):
+            cur1 = self.fc1(x)
+            spk1, mem1 = self.lif1(cur1, mem1)
+            cur2 = self.fc2(spk1)
+            spk2, mem2 = self.lif2(cur2, mem2)
+            spk2_rec.append(spk2)
+            mem2_rec.append(mem2)
+
+        return torch.stack(spk2_rec, dim=0), torch.stack(mem2_rec, dim=0)
+
