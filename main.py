5ee154265fbc70954cecd83da003c190712a8fa4
hasithbasnayake <hasith@ucsb.edu>
2025-03-06 19:51:26 -0800

    IT WORKKSSS!!!! (Kind of) The stpd learning rule is succesfully updating the weights, and when printing out the neuron receptive fields at the end of the model run, I do see that they are being tuned towards certain input features. It's not entirely clear yet which ones (maybe try with mnist to see before moving to fashion mnist). But yeah, that fact that the weight update works, the model is running, and somewhat learning unsupervised is great. Now I just need to do a hyperparameter search, try mnist to see if these receptive fields are actually picking up useful information from the images, and also look back at the paper to see how they reconstructed the neuron's receptive fields.

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ ang = np.ceil(dim / ppa)
 ctr = (1/3) * dim[0]
 sur = (2/3) * dim[0]
 
-n_train = 100
+n_train = 500
 n_test = 200
 r_seed = 2021
 
@@ -42,31 +42,101 @@ num_output = 10
 
 # Spiking Dynamics 
 
-beta = 0.8
-threshold = 1
+beta = 0.1
+threshold = 0.5
 reset_mechanism = "zero"
 
 # Hyperparameters
 
-num_epochs = 2
+num_epochs = 1
 num_steps = 255
 
 train_loader = DataLoader(convON_train, batch_size= 1, shuffle=True)
 
 net = Net(num_input, num_output, beta, threshold, reset_mechanism)
+trace_pre = None
+trace_post = None
 
 for epoch in range(num_epochs):
     iter_counter = 0
+
+    org_weights = net.fc1.weight.data.cpu().numpy()
+    # fig, axes = plt.subplots(2, 5, figsize=(10, 5))
+
+    # weights = net.fc1.weight.data.cpu().numpy()
+
+    # for i, ax in enumerate(axes.flat):
+    #     img = weights[i].reshape(28, 28)  # Reshape each row into 28x28
+    #     ax.imshow(img, cmap="viridis")
+    #     ax.set_title(f"Neuron {i}")
+    #     ax.axis("off")
+
+    # plt.suptitle("Receptive Fields of Output Neurons")
+    # plt.show()
     
+
     for img, label in train_loader:
         flat_img = torch.flatten(img, start_dim=1)
         spk_img = spikegen.latency(flat_img, num_steps = num_steps, normalize = True, linear= True)
-
+        
         spk_rec, mem_rec = net(spk_img)
+
+        trace_pre, trace_post, delta_w = stdp_linear_single_step(
+            fc = net.fc1,
+            in_spike = spk_img.squeeze(1),
+            out_spike = spk_rec.squeeze(1),
+            trace_pre = trace_pre,
+            trace_post = trace_post,
+            tau_pre = 20,
+            tau_post = 20,
+            f_pre = lambda x: x,
+            f_post = lambda x: x,
+        )
+
+        # print(f"Spikes Input: {spk_img.sum().item()}, Output: {spk_rec.sum().item()}")
+        # print(f"trace_pre: {trace_pre.sum().item()}, trace_post: {trace_post.sum().item()}")
+        # print(f"Delta_w Min: {delta_w.min().item()}, Max: {delta_w.max().item()}")
+        
+        with torch.no_grad():
+            net.fc1.weight += delta_w
+
         iter_counter += 1
         print(f"Image: {iter_counter}")
 
-    
+    weights = net.fc1.weight.data.cpu().numpy()
+
+    plt.figure(figsize=(12, 5))
+    plt.imshow(weights, cmap="viridis", aspect="auto")
+    plt.colorbar(label="Weight Value")
+    plt.xlabel("Input Neurons (Flattened Pixels)")
+    plt.ylabel("Output Neurons")
+    plt.title("STDP Weight Matrix Visualization")
+    plt.show()
+
+    dif_weights = org_weights - weights
+
+    plt.figure(figsize=(12, 5))
+    plt.imshow(dif_weights, cmap="viridis", aspect="auto")
+    plt.colorbar(label="Weight Value")
+    plt.xlabel("Input Neurons (Flattened Pixels)")
+    plt.ylabel("Output Neurons")
+    plt.title("STDP Weight Matrix Visualization")
+    plt.show()
+
+    fig, axes = plt.subplots(2, 5, figsize=(10, 5))
+
+    weights = net.fc1.weight.data.cpu().numpy()
+
+    for i, ax in enumerate(axes.flat):
+        img = weights[i].reshape(28, 28)  # Reshape each row into 28x28
+        ax.imshow(img, cmap="viridis")
+        ax.set_title(f"Neuron {i}")
+        ax.axis("off")
+
+    plt.suptitle("Receptive Fields of Output Neurons")
+    plt.show()
+
+        
         
 
 
